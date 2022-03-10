d2aaa8ef21d018694d342e1877ba562cae60e323
Vahe Mkrtchyan <martuni3vahe@gmail.com>
2022-03-10 21:03:54 +0400

    deleted useless mapers

--- LML.NPOManagement.Bll/Services/UserInventoryService.cs
+++ LML.NPOManagement.Bll/Services/UserInventoryService.cs
@@ -17,38 +17,8 @@ namespace LML.NPOManagement.Bll.Services
         {
             var config = new MapperConfiguration(cfg =>
             {
-                cfg.CreateMap<AccountProgress, AccountProgressModel>();
-                cfg.CreateMap<Attachment, AttachmentModel>();
-                cfg.CreateMap<DailySchedule, DailyScheduleModel>();
-                cfg.CreateMap<Donation, DonationModel>();
-                cfg.CreateMap<Account, AccountModel>();
-                cfg.CreateMap<InvestorInformation, InvestorInformationModel>();
-                cfg.CreateMap<InventoryType, InventoryTypeModel>();
-                cfg.CreateMap<MeetingSchedule, MeetingScheduleModel>();
-                cfg.CreateMap<Notification, NotificationModel>();
-                cfg.CreateMap<NotificationType, NotificationTypeModel>();
-                cfg.CreateMap<Template, TemplateModel>();
-                cfg.CreateMap<TemplateType, TemplateTypeModel>();
-                cfg.CreateMap<UserInformation, UserInformationModel>();
                 cfg.CreateMap<UserInventory, UserInventoryModel>();
-                cfg.CreateMap<UserType, UserTypeModel>();
-                cfg.CreateMap<WeeklySchedule, WeeklyScheduleModel>();
-                cfg.CreateMap<AccountProgressModel, AccountProgress>();
-                cfg.CreateMap<AttachmentModel, Attachment>();
-                cfg.CreateMap<DailyScheduleModel, DailySchedule>();
-                cfg.CreateMap<DonationModel, Donation>();
-                cfg.CreateMap<AccountModel, Account>();
-                cfg.CreateMap<InvestorInformationModel, InvestorInformation>();
-                cfg.CreateMap<InventoryTypeModel, InventoryType>();
-                cfg.CreateMap<MeetingScheduleModel, MeetingSchedule>();
-                cfg.CreateMap<NotificationModel, Notification>();
-                cfg.CreateMap<NotificationTypeModel, NotificationType>();
-                cfg.CreateMap<TemplateModel, Template>();
-                cfg.CreateMap<TemplateTypeModel, TemplateType>();
-                cfg.CreateMap<UserInformationModel, UserInformation>();
                 cfg.CreateMap<UserInventoryModel, UserInventory>();
-                cfg.CreateMap<UserTypeModel, UserType>();
-                cfg.CreateMap<WeeklyScheduleModel, WeeklySchedule>();
             });
             _mapper = config.CreateMapper();
         }
