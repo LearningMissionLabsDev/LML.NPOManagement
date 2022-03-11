54214d60908c627d2a6df39269e56469245654e0
Mher <lalazaryan91@inbox.ru>
2022-03-11 22:56:52 +0400

    changeis UserService

---
+++ LML.NPOManagement.Bll/Model/StatusEnumModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LML.NPOManagement.Bll.Model
+{
+    public enum StatusEnumModel
+    {
+        Pending,
+        Activ,
+        Suspending,
+        Closed,
+    }
+}
