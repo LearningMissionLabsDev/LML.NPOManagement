59ff79d870335014872cf79c6554d69688a6ae08
Gavril Alaverdyan <alaverdyangavril@gmail.com>
2022-06-11 00:33:31 +0400

    Create accuntcontroller and accountservice methods async

--- LML.NPOManagement.Bll/Interfaces/IAccountService.cs
+++ LML.NPOManagement.Bll/Interfaces/IAccountService.cs
@@ -4,12 +4,12 @@ namespace LML.NPOManagement.Bll.Interfaces
 {
     public interface IAccountService
     {
-        public IEnumerable<AccountModel> GetAllAccounts();
-        public AccountModel GetAccountById(int id);
+        Task <List<AccountModel>> GetAllAccounts();
+        Task<AccountModel> GetAccountById(int id);
         public int AddAccount(AccountModel accountModel);
         public int ModifyAccount(AccountModel accountModel, int id);
         public void DeleteAccount(int id);
-        public UserIdeaModel AddUserIdea(UserIdeaModel userIdeaModel);
-        public List<UserIdeaModel> GetAllIdea();
+        Task<UserIdeaModel> AddUserIdea(UserIdeaModel userIdeaModel);
+        Task<List<UserIdeaModel>> GetAllIdea();
     }
 }
