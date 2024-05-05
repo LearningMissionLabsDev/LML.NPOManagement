f82eabba4b7ff37603e7da857d9cb30ed98f24e0
vladabrahamian <vladabrahamyan96@gmail.com>
2024-05-06 00:17:36 +0400

    Implemented unit tests for AccountRepository functionalities

--- LML.NPOManagement.Dal/Repositories/Interfaces/IAccountRepository.cs
+++ LML.NPOManagement.Dal/Repositories/Interfaces/IAccountRepository.cs
@@ -9,14 +9,13 @@ namespace LML.NPOManagement.Dal.Repositories.Interfaces
         Task<AccountModel> GetAccountById(int accountId);
         Task<List<AccountModel>> GetAccountsByName(string name);
         Task<List<UserModel>> GetUsersByAccount(int userId);
-        Task<List<AccountUserActivityModel>> GetAccountRoleProgress(int accountId, int accountRoleId);
+        Task<List<AccountUserActivityModel>> GetAccountRoleProgress(int accountId);
         Task<List<Account2UserModel>> GetAccount2Users();
-        Task<AccountModel> AddAccount(AccountModel accountModel);
+        Task<AccountModel> AddAccount(AccountModel accountModel); 
         Task<bool> AddUserToAccount(Account2UserModel account2UserModel);
         Task<AccountUserActivityModel> AddAccountUserActivityProgress(AccountUserActivityModel accountUserActivityModel);
         Task<AccountModel> ModifyAccount(AccountModel accountModel);     
         Task<AccountModel> RemoveUserFromAccount(int accountId, int userId);
-        Task<bool> DeleteAccount(int accountId);       
-        
+        Task<bool> DeleteAccount(int accountId);
     }
 }
