9bd71c607f7571cf532b2a061b5989520295b963
Tik01 <sarukhanyantik@gmail.com>
2024-08-20 05:17:33 +0400

    Update GetUserById

--- LML.NPOManagement/Controllers/UserController.cs
+++ LML.NPOManagement/Controllers/UserController.cs
@@ -106,7 +106,6 @@ namespace LML.NPOManagement.Controllers
         }
 
         [HttpGet("{userId}")]
-        [Authorize(RoleAccess.AllAccess)]
         public async Task<ActionResult<UserCredentialResponse>> GetUserbyId(int userId)
         {
             if (userId <= 0)
