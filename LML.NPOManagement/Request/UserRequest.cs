ba2e2fd93993a344e13a210230c138ec1cfc9457
Tik01 <sarukhanyantik@gmail.com>
2024-10-05 22:54:57 +0400

    Password REGEX change

--- LML.NPOManagement/Request/UserRequest.cs
+++ LML.NPOManagement/Request/UserRequest.cs
@@ -5,7 +5,7 @@ namespace LML.NPOManagement.Request
     public class UserRequest : LoginRequest
     {
         [Required]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{6,25}$",
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,25}$",
         ErrorMessage = "Password must be between 6 and 25 characters and contain one uppercase letter, one lowercase letter, one digit and one special character.")]
         public string ConfirmPassword { get; set; }
     }
